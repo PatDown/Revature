9d95c90815f2c5308f574086d1da80863b4f67db
Pat Downton <pjdownton5017@gmail.com>
2021-08-18 11:26:24 -0500

    Completed the movement from one room to the next

--- eclipse-workspace/HomeTour/src/game/RoomManager.java
+++ eclipse-workspace/HomeTour/src/game/RoomManager.java
@@ -1,6 +1,6 @@
 package game;
 
-import fixtures.Room;
+import fixtures.*;
 import java.io.*;
 import java.util.*;
 
@@ -9,19 +9,25 @@ import java.util.*;
  * @author Pat Down
  */
 public class RoomManager {
-    private Room startingRoom;
-    private final Room[] rooms = new Room[16];
-    public final Map<String, Room> housePlan = new HashMap<>();
-    public Map<String, String[]> exitMap = new HashMap<>();
+    private static Room startingRoom;
+    private static final Map<String, Room> rooms = new HashMap<>();
+    private static final Map<String, String[]> exitMap = new HashMap<>();
     private Scanner fileScanner;
     private final String path = new File("src/game/Rooms.txt").getAbsolutePath();
 
     public Room getStartingRoom() {
         return startingRoom;
     }//getStartingRoom
-
+    
+    public Map<String, String[]> getExitMap(){
+        return exitMap;
+    }//getExitMap
+    
+    public Map<String, Room> getRooms(){
+        return rooms;
+    }//getRooms
+    
     public void init() throws NullPointerException{
-        exitMap = new HashMap<>();
         try {
             fileScanner = new Scanner(new File(path));
         }catch(FileNotFoundException e){
@@ -30,7 +36,7 @@ public class RoomManager {
         Room foyer = new Room(
             fileScanner.nextLine().trim(),
             fileScanner.nextLine().trim(),
-            fileScanner.nextLine().translateEscapes());
+            fileScanner.nextLine().trim().translateEscapes());
         String exits = fileScanner.nextLine();
         Scanner s = new Scanner(exits);
         s.useDelimiter(",");
@@ -39,26 +45,26 @@ public class RoomManager {
         while (s.hasNext()){
             exitsArray[exitNum++] = s.next().trim();
         }//while
-        this.rooms[0] = foyer;
-        this.startingRoom = foyer;
-        this.housePlan.put(foyer.getName(), foyer);
+        startingRoom = foyer;
+        rooms.put(foyer.getName(), foyer);
+        exitMap.put(foyer.getName(), exitsArray);
         buildHouse();
-        for (var e : housePlan.entrySet()){
-            System.out.println(e.getKey());
-            e.getValue().printExits();
+        for (var i : rooms.values()){
+            for (var j : exitMap.entrySet()){
+                if (i.getName().equals(j.getKey()))
+                    i.setExits(j.getValue());
+            }//for
         }//for
         fileScanner.close();
     }//init
     
-    public void buildHouse() throws NullPointerException{
-        String name, sDescription, lDescription;
-        
+    public void buildHouse() throws NullPointerException{        
         while (fileScanner.hasNext()){
             fileScanner.nextLine();
-            name = fileScanner.nextLine().trim();
-            sDescription = fileScanner.nextLine().trim();
-            lDescription = fileScanner.nextLine().translateEscapes();
-            this.housePlan.put(name, new Room(name,sDescription, lDescription));
+            String name = fileScanner.nextLine().trim();
+            String sDescription = fileScanner.nextLine().trim();
+            String lDescription = fileScanner.nextLine().trim().translateEscapes();
+            rooms.put(name, new Room(name,sDescription, lDescription));
             String exits = fileScanner.nextLine();
             Scanner s = new Scanner(exits);
             s.useDelimiter(",");
@@ -67,10 +73,8 @@ public class RoomManager {
             while (s.hasNext()){
                 exitsArray[exitNum++] = s.next().trim();
             }//while
-            this.exitMap.put(name, exitsArray);
+            exitMap.put(name, exitsArray);
         }//while
-        for (var i : exitMap.entrySet()){
-            this.housePlan.get(i.getKey()).setExits(i.getValue());
-        }//for
+        
     }//buildHouse
 }//RoomManager
