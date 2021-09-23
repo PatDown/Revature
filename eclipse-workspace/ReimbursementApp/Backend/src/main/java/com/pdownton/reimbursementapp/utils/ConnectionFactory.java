9ed7d2dd3d7b251ca4f8b67e86e29872c110c77f
Pat Downton <pjdownton5017@gmail.com>
2021-09-23 13:40:29 -0500

    Changes

---
+++ eclipse-workspace/ReimbursementApp/Backend/src/main/java/com/pdownton/reimbursementapp/utils/ConnectionFactory.java
@@ -0,0 +1,38 @@
+package com.pdownton.reimbursementapp.utils;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ *
+ * @author Pat Down
+ */
+public class ConnectionFactory {
+private static Connection connection;
+    
+    private ConnectionFactory(){
+        super();
+    }//ConnectionFactory()
+    
+    public static Connection getConnection(){
+        if (connection == null){
+            try {
+                Properties props = new Properties();
+                FileReader connProps = new FileReader("src/main/resources/connection.properties");
+                props.load(connProps);
+                
+                String connString = 
+                        props.getProperty("url") + "?user=" +
+                        props.getProperty("username") + "&password=" +
+                        props.getProperty("password");
+                
+                connection = DriverManager.getConnection(connString);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } //catch(Exception)
+        }//if (connection == null)
+        return connection;
+    }//getConnection()
+}//ConnectionFactory
